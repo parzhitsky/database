50e663fd9b2bbd925ed5b91e29528c62ec99f5c5
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-02-11 00:18:52 +0200

    Show usage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-export default null;
+import assert from "assert";
+import Database from "./database";
+
+interface Person {
+	name: string;
+	age: number;
+}
+
+const db = new Database();
+const table = db.createTable<Person>("Person");
+
+const { key: johnKey } = table.create({
+	name: "John",
+	age: 42,
+});
+
+table.update(johnKey, (john) => {
+	if (john == null)
+		throw new Error("Expected to see existing entry");
+
+	return { ...john, age: john.age + 1 };
+});
+
+{
+	const entry = table.read(johnKey);
+
+	if (!entry.exists)
+		throw new Error("Expected to see existing entry");
+
+	else
+		assert.strictEqual(entry.value!.age, 43, "John's age should have been equal to 43");
+}
+
+table.delete(johnKey);
+
+if (table.read(johnKey).exists)
+	throw new Error("Expected entry to be deleted");
